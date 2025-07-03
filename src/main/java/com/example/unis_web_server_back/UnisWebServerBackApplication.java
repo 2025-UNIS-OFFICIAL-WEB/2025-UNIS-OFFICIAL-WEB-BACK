76b3b0400aaeadff74e032fed575b21f82d6e2ad
gyesswhat <h2weirdo@icloud.com>
2025-07-03 20:38:17 +0900

    chore: 기본 파일 세팅

---
+++ src/main/java/com/example/unis_web_server_back/UnisWebServerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.unis_web_server_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisWebServerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisWebServerBackApplication.class, args);
+	}
+
+}
