e1ed344e5b511e2b9a7e8a0ef6271b0e9c6e22c1
gyesswhat <h2weirdo@icloud.com>
2025-07-05 23:00:45 +0900

    fix: Swagger 프로파일 명시해서 admin 서버와 api 서버 분리

--- api-server/src/main/java/com/unis/api/config/SwaggerConfig.java
+++ api-server/src/main/java/com/unis/api/config/SwaggerConfig.java
@@ -6,9 +6,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
+@Profile("api")
 @Configuration
 @OpenAPIDefinition(
     info = @Info(
