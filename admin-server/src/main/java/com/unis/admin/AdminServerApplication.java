859b4f64866e6ba8b51f6a6d854a5c1e18aed486
gyesswhat <h2weirdo@icloud.com>
2025-07-06 18:49:45 +0900

    fix: common 모듈 못 읽는 문제 해결을 위해 컴포넌트 스캔 추가 + 컴포넌트 스캔을 위해 JPA 의존성 각 모듈에 추가

--- admin-server/src/main/java/com/unis/admin/AdminServerApplication.java
+++ admin-server/src/main/java/com/unis/admin/AdminServerApplication.java
@@ -2,8 +2,12 @@ package com.unis.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.unis.admin", "com.unis.common"})
+@EnableJpaRepositories(basePackages = "com.unis.common.repository")
+@EntityScan(basePackages = "com.unis.common.domain")
 public class AdminServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class, args);
--- api-server/src/main/java/com/unis/api/ApiServerApplication.java
+++ api-server/src/main/java/com/unis/api/ApiServerApplication.java
@@ -2,8 +2,12 @@ package com.unis.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.unis.api", "com.unis.common"})
+@EnableJpaRepositories(basePackages = "com.unis.common.repository")
+@EntityScan(basePackages = "com.unis.common.domain")
 public class ApiServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiServerApplication.class, args);
