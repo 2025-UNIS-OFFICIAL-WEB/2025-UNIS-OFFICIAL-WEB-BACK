10cf4b64e66090268ee21c308daf8f6ec9c26d6f
gyesswhat <h2weirdo@icloud.com>
2025-07-06 19:11:28 +0900

    fix: SwaggerConfig 수정해 Authorization header 붙지 않는 에러 수정

--- admin-server/src/main/java/com/unis/admin/config/SwaggerConfig.java
+++ admin-server/src/main/java/com/unis/admin/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.unis.admin.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,9 +24,21 @@ import java.util.List;
 )
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
             .servers(List.of(new Server().url("https://admin-unis.com")));
     }
 }
